4b1f47829b882ff150240c935f04dcb32103fd9c
szyszak <krzszyszko@gmail.com>
2019-07-03 21:41:15 +0200

    Updates

---
+++ precache-manifest.5c0b7c37752168a83583d5391dda2536.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4773148e4c24f6aef55a908b7acd519a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89c8fa817a903da530a7",
+    "url": "/static/js/2.79b24766.chunk.js"
+  },
+  {
+    "revision": "852ac1bd6ed3c8909415",
+    "url": "/static/js/main.6eaf2118.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
